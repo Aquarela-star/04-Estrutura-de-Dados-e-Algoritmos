eec34be5001e775d68422c49e3760b927e04d15f
Rayssa Fidelis <rayssaif0022@gmail.com>
2026-08-13 01:04:15 +0000

    aulalista

---
+++ main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Principal{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        Pessoa obj1 = new Pessoa()
+    }
+}
