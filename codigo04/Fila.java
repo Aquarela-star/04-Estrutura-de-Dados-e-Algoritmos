dfada11333dbc894787a920833e386eed4f82520
Rayssa Fidelis <rayssaif0022@gmail.com>
2026-09-03 01:20:00 +0000

    codigo04

---
+++ codigo04/Fila.java
@@ -0,0 +1,10 @@
+package codigo04;
+
+public class Fila {
+    private String nomeFila;
+    private No<T> primeiroNo;
+    private No<T> ultimoNo;
+
+
+    public fila
+}
---
+++ codigo04/Principal.java
@@ -0,0 +1,8 @@
+public class Principal{
+    public static void main(String[] args){
+        Fila<String> pilha = new
+        Fila<String>("Letras");
+
+    }
+}
+
